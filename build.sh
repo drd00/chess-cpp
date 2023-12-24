f3bb50e1ce42dc5a7b28c6fc20c66a3120991e58
daniel <daniel@danield.home>
2023-12-24 16:14:05 +0000

    Fixed error in the build.sh script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 GTEST_LIB_DIR="./Google_tests/lib"
 GTEST_GIT="https://github.com/google/googletest.git"
 
-if [ -d "$GTEST_LIB_DIR"]; then
+if [ -d "$GTEST_LIB_DIR" ]; then
   echo "GTEST_LIB_DIR exists."
 else
   echo "GTEST_LIB_DIR does not exist. Cloning."
