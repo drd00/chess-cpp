3b2a7b5155d0f78c076958e8df39d9204a304aee
daniel <daniel@danield.home>
2023-12-24 16:08:10 +0000

    Added build.sh file and some different CMake files to .gitignore

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GTEST_LIB_DIR="./Google_tests/lib"
+GTEST_GIT="https://github.com/google/googletest.git"
+
+if [ -d "$GTEST_LIB_DIR"]; then
+  echo "GTEST_LIB_DIR exists."
+else
+  echo "GTEST_LIB_DIR does not exist. Cloning."
+  git clone $GTEST_GIT $GTEST_LIB_DIR
+fi
+
+cmake CMakeLists.txt
+make
+
